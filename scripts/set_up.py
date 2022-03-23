ae40fb2175793d3a9b0b50c108679f4a1a6c4546
mahumazeem <azeem.mahum@gmail.com>
2022-03-23 14:42:45 -0700

    Added data files for 3 genres with song titles, links and ids

--- scripts/set_up.py
+++ scripts/set_up.py
@@ -1,5 +1,8 @@
 import requests
 import configparser
+import json
+import pandas as pd
+from pandas import json_normalize 
 
 def authenticate(CLIENT_ID, CLIENT_SECRET):
   AUTH_URL = 'https://accounts.spotify.com/api/token'
@@ -12,15 +15,42 @@ def authenticate(CLIENT_ID, CLIENT_SECRET):
   
   if 'error' in resp.keys():
     print('Failed to authenticate.')
-    return False
+    return ''
   print('Successfully authenticated.')
-  return True
+  return resp['access_token']
+
+def getSeedData(headers, genre, min_pop, max_pop):
+  tracks=[]
+  SEED_URL = 'https://api.spotify.com/v1/recommendations?limit=100&seed_genres=' + genre + '&min_popularity=' + min_pop + '&max_popularity=' + max_pop
+  response = requests.get(SEED_URL, headers=headers)
+  results = json.loads(response.text)
+  x  = json_normalize(results['tracks'])
+  return x
 
 
+
+genres = ['country','pop', 'jazz']
 parser = configparser.ConfigParser()
 parser.read("config.txt")
+
 
 CLIENT_ID = parser.get("config", "client_id")
 CLIENT_SECRET = parser.get("config", "client_secret")
 
-authenticate(CLIENT_ID, CLIENT_SECRET)
+auth_token = 'Bearer ' + authenticate(CLIENT_ID, CLIENT_SECRET)
+headers = {"Authorization": auth_token}
+
+COLS = ['id', 'name', 'popularity', 'uri', 'href', 'artists']
+for genre in genres:
+  name = genre + '.csv'
+  tracks=(getSeedData(headers, genre, '0','10'))
+  tracks = tracks.append(getSeedData(headers, genre, '11','25'))
+  tracks = tracks.append(getSeedData(headers, genre, '26','50'))
+  tracks = tracks.append(getSeedData(headers, genre, '51','60'))
+  tracks = tracks.append(getSeedData(headers, genre, '61','75'))
+  tracks = tracks.append(getSeedData(headers, genre, '76','100'))
+  tracks = tracks.drop(columns=['artists', 'available_markets', 'disc_number','duration_ms','explicit', 'is_local', 'preview_url','track_number','type', 'album.album_type','album.artists','album.available_markets','album.external_urls.spotify','album.href','album.id','album.images','album.release_date','album.release_date_precision','album.total_tracks','album.type','album.uri','external_ids.isrc','external_urls.spotify'])
+  tracks.to_csv(name, index=False)
+
+# url ='https://api.spotify.com/v1/recommendations/available-genre-seeds'
+# response = requests.get(url, headers=headers)
